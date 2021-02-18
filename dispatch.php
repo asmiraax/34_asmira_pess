f1dd244dce6a0b9e615f4f1ff6d8f2514db44250
asmiraax <asmira212524@gmail.com>
2021-02-18 12:38:57 +0800

    third version
    added some codes

--- dispatch.php
+++ dispatch.php
@@ -1,3 +1,56 @@
+<?php
+  $callerName = $_POST["callerName"];
+  $contactNo = $_POST["contactNo"];
+  $locationOfIncident = $_POST["locationOfIncident"];
+  $typeOfIncident = $_POST["typeOfIncident"];
+  $descriptionOfIncident = $_POST["descriptionOfIncident"];
+
+	
+ require_once "db.php";
+ $conn = new mysqli(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+ $sql = "SELECT patrolcar.patrolcar_id,patrolcar_status.patrolcar_status_desc FROM `patrolcar` INNER JOIN patrolcar_status ON patrolcar.patrolcar_status_id = patrolcar_status.patrolcar_status_id";
+ $result = $conn->query($sql);
+ $cars = [];
+ while($row = $result->fetch_assoc()) {
+  $id = $row["patrolcar_id"];
+  $status = $row["patrolcar_status_desc"]; 
+  $car = ["id"=>$id, "status"=>$status];
+  array_push($cars,$car);
+ }
+ $conn->close();
+
+  $btnDispatchClicked = isset($_POST["btnDispatch"]);
+  $btnProcessCallClicked = isset($_POST["btnProcessCall"]);
+  if($btnDispatchClicked == false && $btnProcessCallClicked == false) {
+}
+if($$btnDispatchCallClicked == true) {
+	$insertIncidentSuccess = false;
+	$hasCarSelection = isset($_POST["cbCarSelection"]);
+	$patrolcarDispatched = [];
+	$numOfPatrolCarDispatched = 0;
+	if($hasCarSelection == true) {
+	$patrolcarDispatched = $_POST["cbCarSelection"];
+	$numOfPatrolCarDispatched = count($patrolcarDispatched);
+	}
+	
+	$incidentStatus = 0;
+	
+	if($numOfPatrolCarDispatched > 0) {
+		$incidentStatus = 2; //dispatched
+	}
+	else {
+		$incidentStatus = 1; //pending
+	}
+  $callerName = $_POST["callerName"];
+  $contactNo = $_POST["contactNo"];
+  $locationOfIncident = $_POST["locationOfIncident"];
+  $typeOfIncident = $_POST["typeOfIncident"];
+  $descriptionOfIncident = $_POST["descriptionOfIncident"];
+	
+	$sql ="INSERT INTO `incident`( `caller_name`, `phone_number`, `incident_type_id`, `incident_location`, `incident_desc`, `incident_status_id`, `time_called`) VALUES ( '" . $callerName ."','" . $contactNo . "','". $typeOfIncident . "','". $locationOfIncident . "', '" .$descriptionOfIncident ."','". $incidentStatus . "',now())";
+	echo $sql;
+  
+?>
 <!doctype html>
 <html>
 <head>
@@ -13,13 +66,13 @@
 	include "header.php";
 	?>
   <section class="mt-3">
-    <form>
+    <form action="<?php echo htmlentities($_SERVER["PHP_SELF"]) ?>" method="post">
       <div class="form-group row">
         <label for="callerName" class="col-sm-4 col-form-label">Caller's Name</label>
         <div class="col-sm-8">
 			<span>
-			David 
-			<input type="hidden"  id="callerName" name="callerName" value="David">
+			<?php echo $callerName; ?>
+			<input type="hidden"  id="callerName" name="callerName" value="<?php echo $callerName; ?>">
 			</span>
         </div>
       </div>
@@ -27,8 +80,8 @@
         <label for="contactNo" class="col-sm-4 col-form-label">Contact No</label>
         <div class="col-sm-8">
 			<span>
-			91234567
-			<input type="hidden" id="contactNo" name="contactNo" value="91234567">
+			<?php echo $contactNo; ?>
+			<input type="hidden" id="contactNo" name="contactNo" value="<?php echo $contactNo; ?>">
 			</span>
         </div>
       </div>
@@ -36,8 +89,8 @@
         <label for="locationOfIncident" class="col-sm-4 col-form-label">Location of Incident</label>
         <div class="col-sm-8">
 			<span>
-				CCK
-             <input type="hidden" id="locationOfIncident" name="locationOfIncident" value="CCK">			
+				<?php echo $locationOfIncident; ?>
+             <input type="hidden" id="locationOfIncident" name="locationOfIncident" value="<?php echo $locationOfIncident; ?>">			
 			</span>
           </div>
       </div>
@@ -45,9 +98,9 @@
         <label for="typeOfIncident" class="col-sm-4 col-form-label">Type of Incident</label>
         <div class="col-sm-8">
 			<span>
-			Fire
+			<?php echo $typeOfIncident; ?>
 			 <input id="typeOfIncident" type="hidden" 
-		     name="typeOfIncident" value="Fire">
+		     name="typeOfIncident" value="<?php echo $typeOfIncident; ?>">
 			</span>
         </div>
       </div>
@@ -55,11 +108,11 @@
         <label for="descriptionOfIncident" class="col-sm-4 col-form-label">Description of Incident</label>
         <div class="col-sm-8">
 			<span>
-			  Fire at CCK
+			  <?php echo $descriptionOfIncident; ?>
 		<input name="descriptionOfIncident"
 				 type="hidden"
 				   id="descriptionOfIncident"
-				   value="Fire at CCK">
+				   value="<?php echo $descriptionOfIncident; ?>">
 			</span>
         </div>
       </div>
@@ -73,34 +126,25 @@
 				<th>Status</th>
 				<th></th>
 			</tr>
-			<tr>
-				<td>SJA6672A</td>
-				<td>Free</td>
-				<td>
-				<input type="checkbox" name="cbCarSelection[]">
-				</td>
-			</tr>
-				<tr>
-				<td>SGA8765A</td>
-				<td>Free</td>
-				<td>
-				<input type="checkbox" name="cbCarSelection[]">
-				</td>
-			</tr>
-				<tr>
-				<td>SJE5564A</td>
-				<td>Free</td>
-				<td>
-				<input type="checkbox" name="cbCarSelection[]">
-				</td>
-			</tr>
+			<?php
+				 foreach($cars as $car){
+					 echo "<tr>" .
+				              "<td>" . $car["id"] . "</td>" .
+				              "<td>" . $car["status"] . "</td>" .
+				              "<td>" .
+				             "<input type=\"checkbox\" " . "name=\"cbCarSelection[]\">" .
+				              "</td>" .
+			             "</tr>";
+				 }
+				?>
+				
 			</tbody>
 		</table>
         </div>
       </div>
        <div class="form-group row">
         <div class="offset-sm-4 col-sm-8">
-      <button type="submit" class="btn btn-primary" name="submit" id="submit">Dispatch</button>
+      <button type="submit" class="btn btn-primary" name="btnDispatch" id="submit">Dispatch</button>
         </div>
       </div>
      
