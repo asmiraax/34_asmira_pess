ec657e2c6d8a4765d768c4983a1b1799967f8a96
asmiraax <asmira212524@gmail.com>
2021-03-02 16:54:51 +0800

    updated

--- dispatch.php
+++ dispatch.php
@@ -62,13 +62,26 @@ if ( $btnDispatchClicked == true ) {
 
   foreach ( $patrolcarDispatched as $eachCarId ) {
     //echo $eachCarId . "<br>";
-  }
+  
   $sql = "UPDATE `patrolcar` SET `patrolcar_status_id`=1 WHERE 'patrolcar_id'='" . $eachCarId . "'";
   $updateSuccess = $conn->query($sql);
 
   if ( $updateSuccess == false ) {
     echo "Error:" . $sql . "<br>" . $conn->error;
   }
+	 
+   $sql = "INSERT INTO `dispatch` ('incident_id', 'patrolcar_id', 'time_dispatched') VALUES (" . $incidentId . ",'" . $eachCarId . "',now())";
+   $insertDispatchSuccess = $conn->query($sql);
+   
+   if ( $insertDispatchSuccess == false ) {
+      echo "Error:" . $sql . "<br>" . $conn->error;
+  }   
+  }
+	$conn->close();
+	
+	if($insertDispatchSuccess == true && $updateSuccess == true && $insertDispatchSuccess == true) {
+		header("location: logcall.php");
+	}
 }
 ?>
 <!doctype html>
